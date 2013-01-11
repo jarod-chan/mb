2f451ef97cb01bcfdbbb667ed29b1c5d5ab29948
jhon_chen <jhon.chen@gmail.com>
2013-01-11 15:51:35 +0800

    事务测试通过

--- src/main/java/cn/fyg/mb/domain/UserMapper.java
+++ src/main/java/cn/fyg/mb/domain/UserMapper.java
@@ -4,18 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface UserMapper {
 	
 	public List<HashMap<String,Object>> getAll();
 	
-	@Transactional
+
 	public void save(User user);
 	
 	public User find(String key_);
 	
-	@Transactional
 	public void delete(String key_);
 }
